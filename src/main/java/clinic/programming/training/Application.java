2ace5863a55b1b46e04095f31e1bca1cc0c449ce
Lehar Bhandari <Lehar Bhandari>
2018-10-28 22:52:23 +0530

    Adding StringUtils and its dependency in pom file

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,9 +1,15 @@
 package clinic.programming.training;
 import java.util.List;
 import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
     
+	public int countWords(String words){
+		String[] separateWords = StringUtils.split(words, ' ');
+		return(separateWords == null ? 0 : separateWords.length);
+		
+	}
 	public void greet(){
 		List<String> list = new ArrayList<>();
 		list.add("Hello!");
